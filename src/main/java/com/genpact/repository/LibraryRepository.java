7489b739215372659a53d99787d95ebac5d98487
sridevi <yellapu.sridevi@gmal.com>
2021-01-28 19:20:46 -0500

    Revert "initial commit"
    This reverts commit 36abfd38

--- src/main/java/com/genpact/repository/LibraryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.genpact.repository;
-
-import com.genpact.domain.Library;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface LibraryRepository extends CrudRepository<Library, Long> {
-}
--- src/main/java/com/genpact/service/LibraryService.java
+++
@@ -1,9 +0,0 @@
-package com.genpact.service;
-
-import com.genpact.domain.BookData;
-
-import java.util.Map;
-
-public interface LibraryService {
-    Map<String, Object> getAllBooks(Long libraryId);
-}
